807ea0fea817cd6475989b78b3648ca1bd9ff915
mingfei.yao <mingfei.yao@bizfocus.cn>
2024-10-16 17:50:51 +0800

    feat: 开启代码压缩，去除console

--- src/pages/interface/interface.vue
+++ src/pages/interface/interface.vue
@@ -1,11 +1,13 @@
 <template>
   <view>
     <text>接口</text>
+    <button type="primary" @click="fetchAPI">接口请求啦啦啦</button>
+    <button type="warning" @click="getUserInfo">获取用户信息</button>
     <uni-calendar
       :insert="true"
       :lunar="true"
       :start-date="'2019-3-2'"
-      :end-date="'2019-5-20'"
+      :end-date="'2040-5-20'"
       @change="change"
     />
   </view>
@@ -19,6 +21,31 @@ export default {
       uni.showToast({
         title: "按钮被点击了！",
         icon: "none",
+      });
+    },
+    fetchAPI() {
+      uni.request({
+        url: "https://api.apihubs.cn/holiday/get",
+        method: "GET",
+        data: {
+          year: "2021",
+        },
+        success: (res) => {
+          console.log("res", res);
+        },
+        fail: (err) => {
+          console.log("err", err);
+        },
+      });
+    },
+    getUserInfo() {
+      uni.getUserInfo({
+        success: (res) => {
+          console.log("res", res);
+        },
+        fail: (err) => {
+          console.log("err", err);
+        },
       });
     },
   },
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const ReplaceAppIdPlugin = require("./build/replaceAppIdPlugin.js");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   configureWebpack: {
@@ -8,8 +9,27 @@ module.exports = {
         "@": path.resolve(__dirname, "src"), // 根据你的组件目录修改
       },
     },
+    //生产环境开启压缩
+    optimization: {
+      minimize: true, // 启用代码压缩
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: process.env.NODE_ENV === "production", // 仅在生产环境中删除 console.log
+              drop_debugger: process.env.NODE_ENV === "production", // 仅在生产环境中删除 debugger
+            },
+            output: {
+              comments: process.env.NODE_ENV !== "production", // 仅在开发环境中保留注释
+            },
+          },
+          extractComments: false, // 不提取注释
+        }),
+      ],
+    },
     plugins: [new ReplaceAppIdPlugin()],
   },
+
   transpileDependencies: ["@dcloudio/uni-ui"],
 
   devServer: {

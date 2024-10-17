cd0583a71788b692328c922b26dec998f3778321
mingfei.yao <mingfei.yao@bizfocus.cn>
2024-10-17 15:03:17 +0800

    feat: 小程序开启按需加载

--- src/subpages/news/news.vue
+++ src/subpages/news/news.vue
@@ -7,16 +7,11 @@
 <script>
 export default {
   onLoad(options) {
-    console.log("分包新闻页加载");
-    console.log("options", options);
     uni.showToast({
-      title: JSON.stringify(options),
+      title: JSON.stringify(options || { content: "空" }),
       icon: "none",
       mask: true,
     });
-  },
-  onUnload() {
-    console.log("分包新闻页卸载");
   },
 };
 </script>
